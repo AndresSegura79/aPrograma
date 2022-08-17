3fed14dd1fc17b04b4e4590ba6152312f767d68d
AndresSegura79 <edgarandressegurav@gmail.com>
2022-08-17 16:43:13 -0300

    Creo la carpeta BackEnd, con Maven y creacion estructura
    Creo la carpeta del proyecto BackEnd  donde esta el proyecto que cree con Maven y sus dependencias, también creo la BD en MySQL:  backendaprograma y pruebo las conexiones de XAMPP

---
+++ aPrograma/BackEnd/src/main/java/com/portfolio/aprograma/AprogramaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.aprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprogramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprogramaApplication.class, args);
+	}
+
+}
---
+++ aPrograma/BackEnd/src/test/java/com/portfolio/aprograma/AprogramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aprograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprogramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
